66b99b10022f5118f5311e422deb056fa2f1179d
李静波 <lijingbo09@cnpc.com.cn>
2019-09-15 16:42:07 +0800

    activeMQ 对象，生产者，消费者

--- src/main/java/cc/lijingbo/HelloApplication.java
+++ src/main/java/cc/lijingbo/HelloApplication.java
@@ -1,8 +1,12 @@
 package cc.lijingbo;
 
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import javax.jms.Queue;
 
 
 @SpringBootApplication
@@ -11,4 +15,11 @@ public class HelloApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloApplication.class, args);
     }
+
+    @Bean
+    public Queue queue(){
+        return new ActiveMQQueue("active.queue");
+    }
+
+
 }
---
+++ src/main/java/cc/lijingbo/controller/QueueController.java
@@ -0,0 +1,41 @@
+package cc.lijingbo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.jms.Queue;
+
+/**
+ * 北京中油瑞飞信息技术有限责任公司 研究院
+ *
+ * @author : lijingbo
+ * @类:QueueController
+ * @描述:
+ * @版本信息：
+ * @date : 2019-09-15 16:01
+ */
+@RestController
+public class QueueController {
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @RequestMapping("/send")
+    public void send() {
+        String message = "新发送的第 %s 条消息!";
+        for (int i = 0; i < 1000; i++) {
+            jmsMessagingTemplate.convertAndSend(queue, String.format(message, i));
+        }
+    }
+
+    @JmsListener(destination = "active.queue")
+    public void readActiveQueue(String message) {
+        System.out.println("接收到消息：" + message);
+    }
+
+}
