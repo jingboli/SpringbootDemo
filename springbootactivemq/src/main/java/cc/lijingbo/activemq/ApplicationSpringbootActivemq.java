aaba830e5926e91dc28021cd6903b7e87fc8987b
lijingbo09 <cnpcLaoer@321>
2020-03-06 13:53:30 +0800

    把 mybatis 挪到需要用到数据库的 module 里面

--- springbootactivemq/src/main/java/cc/lijingbo/activemq/ApplicationSpringbootActivemq.java
+++ springbootactivemq/src/main/java/cc/lijingbo/activemq/ApplicationSpringbootActivemq.java
@@ -4,10 +4,13 @@ import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.jms.Queue;
 
 @SpringBootApplication
+@RestController
 public class ApplicationSpringbootActivemq {
 
     public static void main(String[] args) {
@@ -17,5 +20,11 @@ public class ApplicationSpringbootActivemq {
     @Bean
     public Queue queue(){
         return new ActiveMQQueue("active.queue");
+    }
+
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello,Redis";
     }
 }
